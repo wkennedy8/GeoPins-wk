494e8751525526a88bf2f1eaf82d11d14ff0588e
williamkennedy <wkennedy8@yahoo.com>
2020-03-17 03:03:57 -0400

    change header styles

--- client/src/withRoot.js
+++ client/src/withRoot.js
@@ -1,6 +1,6 @@
 import React from "react"
 import { MuiThemeProvider, createMuiTheme } from "@material-ui/core/styles"
-import red from "@material-ui/core/colors/red"
+import blue from "@material-ui/core/colors/blue"
 import green from "@material-ui/core/colors/green"
 import CssBaseline from "@material-ui/core/CssBaseline"
 
@@ -9,9 +9,9 @@ import CssBaseline from "@material-ui/core/CssBaseline"
 const theme = createMuiTheme({
 	palette: {
 		primary: {
-			light: red[100],
-			main: red[500],
-			dark: red[700]
+			light: blue[100],
+			main: blue[400],
+			dark: blue[700]
 		},
 		secondary: {
 			light: green[300],
